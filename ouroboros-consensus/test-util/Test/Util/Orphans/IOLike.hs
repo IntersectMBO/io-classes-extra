acc4904536f0f05b56acc5567457f276e789f205
Javier Sagredo <jasataco@gmail.com>
2023-04-24 21:07:33 +0200

    Reorganize subpackages by stanzas

--- ouroboros-consensus/test-util/Test/Util/Orphans/IOLike.hs
+++
@@ -1,7 +0,0 @@
-{-# OPTIONS_GHC -fno-warn-orphans #-}
-module Test.Util.Orphans.IOLike () where
-
-import           Control.Monad.IOSim
-import           Ouroboros.Consensus.Util.IOLike
-
-instance IOLike (SimM s)
